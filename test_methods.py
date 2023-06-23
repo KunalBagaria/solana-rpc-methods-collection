e8a3d6f7149e55e43410b44ecd5407a9aa8d4a55
Kunal Bagaria <61944452+KunalBagaria@users.noreply.github.com>
2023-06-24 00:23:21 +0530

    add: failure on non-200 response

--- test_methods.py
+++ test_methods.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import sys
 
 with open('rpc_methods.json', 'r') as file:
     json_data = json.load(file)
@@ -24,4 +25,8 @@ for index, item in enumerate(items):
     response = requests.request(method, url, headers=headers, data=body)
 
     print("Response Status Code:", response.status_code)
+    if response.status_code != 200:
+        print("Error: HTTP", response.status_code)
+        sys.exit(1)  # this will cause GitHub Actions to fail
+
     print("Response Content:", response.content, "\n")
